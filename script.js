dfa7e9506bfdd51d9f89230e18f228418c421548
Matthew Sayers <saye0059@algonquinlive.com>
2019-09-10 13:58:50 -0400

    Doing coding.

---
+++ script.js
@@ -0,0 +1,9 @@
+const name = 'Matthew Sayers'
+console.log(`Hello, my name is ${name}.`)
+
+const number = 1999
+let result = number * 2
+result *= 5
+result = result / number
+result -= 7
+console.log(result)
